bc2781eee2075bc08f2f48b55ba00ed7b98e1e01
Joshua Stresing <pyrotorch69@yahoo.com>
2022-02-07 18:16:45 -0800

    looking good

--- animals/animals.js
+++ animals/animals.js
@@ -1,12 +1,12 @@
-import { animals } from '..data.js';
-import { findByID } from '..utils.js';
+import { animals } from '../data.js';
+import { findById } from '../utils.js';
 
 const animalName = document.getElementById('animal-name');
 const animalImage = document.getElementById('animal-image');
 const animalSounds = document.getElementById('animal-sounds');
 
 const params = new URLSearchParams(window.location.search);
-const animal = findByID(params.get('id'), animals);
+const animal = findById(params.get('id'), animals);
 
 animalName.textContent = animal.name;
 animalImage.src = `../assets/${animal.type}.svg`;
